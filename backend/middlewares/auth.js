c9b38ef3f975be174b142df4050ec7c0b4014702
Yaroslav Chertov <yachertov@yandex.ru>
2023-07-03 00:11:53 +0300

    исправить после 1 ревью

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,28 +1,3 @@
-// const jwt = require('jsonwebtoken');
-// const config = require('../utils/config');
-// const Unauthorized = require('../utils/errors/unauthorized');
-
-// const { JWT_SECRET = config.jwtSecretKey } = process.env;
-
-// module.exports = (req, res, next) => {
-//   const { authorization } = req.headers;
-//   const bearer = 'Bearer ';
-//   if (!authorization || !authorization.startsWith(bearer)) {
-//     next(new Unauthorized('Необходима авторизация.'));
-//   }
-//   const token = authorization.replace(bearer, '');
-//   let payload;
-
-//   try {
-//     payload = jwt.verify(token, JWT_SECRET);
-//   } catch (err) {
-//     const error = new Unauthorized('Необходима авторизация.');
-//     return next(error);
-//   }
-//   req.user = payload;
-//   return next();
-// };
-
 const jwt = require('jsonwebtoken');
 const Unauthorized = require('../utils/errors/unauthorized');
 
@@ -32,7 +7,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer')) {
-    throw new Unauthorized('Необходима авторизация.');
+    next(new Unauthorized('Необходима авторизация.'));
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -42,7 +17,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    throw new Unauthorized('Необходима авторизация.');
+    next(new Unauthorized('Необходима авторизация.'));
   }
 
   req.user = payload;
