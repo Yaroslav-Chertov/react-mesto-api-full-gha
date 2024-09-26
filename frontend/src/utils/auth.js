89084c710743be0a7899cf73e56c200c2337d698
Yaroslav Chertov <yachertov@yandex.ru>
2024-09-26 14:54:36 +0300

    Update auth.js

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://yaroslav.student-api.nomoreparties.sbs";
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://yaroslav.student-api.nomoreparties.sbs";
+// export const BASE_URL = "http://localhost:3000";
 
 function getJson(res) {
     if (res.ok) {
